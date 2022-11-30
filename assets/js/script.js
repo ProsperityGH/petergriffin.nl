d8c8dabc949000e0f77f6fe67bef84aaf6ba978f
ProsperityGH <90307071+ProsperityGH@users.noreply.github.com>
2022-11-30 08:59:44 +0100

    Peter Griffin has now become spammable
    have fun :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
-document.getElementById("peter").addEventListener("click", function(){
-    document.getElementById("audio").play();
-});
+// document.getElementById("peter").addEventListener("click", function(){
+//     document.getElementById("audio").play();
+// });
+
+(function( d ) {
+    'use strict';
+     var count = 0,
+         audio = d.getElementById( 'audios' ).getElementsByTagName( 'audio' );
+     d.getElementById( 'peter' ).addEventListener( 'click',
+        function() {
+            audio[count].play();
+            count ++;
+            console.log(count);
+       if ( count > 9 ) {
+            count = 0;
+           }          
+          }, false );
+ }( document ));
